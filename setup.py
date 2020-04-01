664a8ab8bbf0ebaa39f94cdbfeacd8c70a4c8f9b
Lautaro Navarro <navarro_lautaro@hotmail.com>
2020-04-01 00:17:58 -0300

    Adding error classes

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='httperrors',
+    packages=['httperrors'],
+    version='1.0.0',
+    license='gpl-3.0',
+    description='Library that provides a easy to use set of http errors along with useful descriptions',  # noqa
+    author='Lautaro Navarro',
+    author_email='navarro_lautaro@hotmail.com',
+    url='https://github.com/LautaroNavarro/httperrors',
+    download_url='https://github.com/LautaroNavarro/httperrors/archive/1.0.0.tar.gz',  # noqa
+    keywords=['HTTP', 'ERROR', 'TEST'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+  ],
+)
